2bb6d366ceeafba14ae7ee3443eed263f42f107b
Jan Vince <jan@vince.cz>
2020-06-22 12:44:07 +0200

    Fixed import function

--- formwidgets/ImportPreset.php
+++ formwidgets/ImportPreset.php
@@ -8,6 +8,7 @@ use October\Rain\Parse\Yaml;
 use Config;
 use Flash;
 use Log;
+use AjaxException;
 
 class ImportPreset extends FormWidgetBase {
 
@@ -29,13 +30,24 @@ class ImportPreset extends FormWidgetBase {
     public function onImportPreset()
     {
 
-        $file = base_path(post('file_name'));
+        if(post('file_name')) {
+
+            $file = storage_path('app/media' . post('file_name'));
+
+        } else {
+
+            $file = base_path(trans('janvince.smallgdpr::lang.formwidgets.importpreset.file_name_default'));
+        }
+        
 
         if (!file_exists($file)) {
 
             Log::error('SG: Preset file was not found!');
             Flash::error(trans('janvince.smallgdpr::lang.formwidgets.importpreset.flash.file_error'));
-            return false;
+            
+            throw new AjaxException([
+                'X_OCTOBER_ERROR_MESSAGE' => trans('janvince.smallgdpr::lang.formwidgets.importpreset.flash.file_error'),
+            ]);
         }
 
         $content = null;
@@ -48,7 +60,10 @@ class ImportPreset extends FormWidgetBase {
 
             Log::error('SG: Error parsing config file! ' . $e->getMessage());
             Flash::error(trans('janvince.smallgdpr::lang.formwidgets.importpreset.flash.parse_error'));
-            return false;
+
+            throw new AjaxException([
+                'X_OCTOBER_ERROR_MESSAGE' => trans('janvince.smallgdpr::lang.formwidgets.importpreset.flash.parse_error'),
+            ]);
         }
 
         try {
@@ -59,7 +74,10 @@ class ImportPreset extends FormWidgetBase {
 
             Log::error('SG: Error importing data! ' . $e->getMessage());
             Flash::error(trans('janvince.smallgdpr::lang.formwidgets.importpreset.flash.import_error'));
-            return false;
+
+            throw new AjaxException([
+                'X_OCTOBER_ERROR_MESSAGE' => trans('janvince.smallgdpr::lang.formwidgets.importpreset.flash.import_error'),
+            ]);
         }
 
         Flash::success(trans('janvince.smallgdpr::lang.formwidgets.importpreset.flash.import_successfull'));
